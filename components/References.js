86c397b88eabbce37acabb3dbd1bd4e737cb9155
Tin Stribor Sohn <54748826+TinStriborDodig@users.noreply.github.com>
2021-09-19 20:18:27 +0200

    intermediate commit fix team members

--- components/References.js
+++ components/References.js
@@ -1,10 +1,14 @@
+import { GitHub, FileText, Linkedin} from 'react-feather';
+
+
 export default function References(props) {
     return(<>
+
             {props.pdf && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.pdf}>PDF</a></span>}
 
-        {props.arxiv && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.arxiv}>Arxiv</a></span>}
-        {props.github && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.github}>Github</a></span>}
-        {props.linkedin && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.linkedin}>Linkedin</a></span>}
+            {props.arxiv && <span class="flex flex-row justify-center items-center p-4 py-2">     <FileText color="#5A005B" size={16}/><a class="text-purple-500 font-semibold text-sm pl-2" href={props.arxiv}>Arxiv</a></span>}
+        {props.github && <span class="flex flex-row justify-center items-center p-4 py-2">     <GitHub color="#5A005B" size={16}/><a class="text-purple-500 font-semibold text-sm pl-2" href={props.github}>Github</a></span>}
+        {props.linkedin && <span class="flex flex-row justify-center items-center p-4 py-2">     <Linkedin color="#5A005B" size={16}/><a class="text-purple-500 font-semibold text-sm pl-2" href={props.linkedin}>Linkedin</a></span>}
         {props.website && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.website}>Website</a></span>}
         {props.googlescholar && <span><a class="text-purple-500 font-semibold text-sm p-8" href={props.googlescholar}>Google Scholar</a></span>}
 
