11139cf99bc76622613c3377d23e5b3bb444df5d
Tin Stribor Sohn <54748826+TinStriborDodig@users.noreply.github.com>
2021-09-22 09:26:35 +0200

    thesis and seminars data all

--- components/NewsGrid.js
+++ components/NewsGrid.js
@@ -3,7 +3,7 @@ import TwitterCard from "./TwitterCard";
 export default function NewsGrid(props) {
 
     return (<div class="flex flex-row justify-center items-center w-full lg:px-16 ">
-        <div id="news" class="grid grid-cols-1    gap-8 p-6 mb-16 ">
+        <div id="news" class="grid grid-cols-2   lg:px-44 py-4 gap-8 w-full mb-16 ">
             <TwitterCard account="ExplainableML"></TwitterCard>
 
         </div></div>)
