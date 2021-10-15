8c1cf36b583c2fd708a4f2148845b84148838d8c
Tin Stribor Sohn <54748826+TinStriborDodig@users.noreply.github.com>
2021-10-15 10:12:35 +0200

    some changes

--- components/ContactGrid.js
+++ components/ContactGrid.js
@@ -1,10 +1,21 @@
 import ContactCard from "./ContactCard";
 import TwitterCard from "./TwitterCard";
+import { useEffect, useState } from 'react';
+
+
 export default function ContactGrid(props) {
+  
+
+
+
+
+
   return (
     <div id="contact" class="container grid grid-cols-1  gap-8 p-6 mb-16">
       
       <ContactCard ></ContactCard>
+
+
     </div>
   );
 }
