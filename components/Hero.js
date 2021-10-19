a97cd0f87f39da4c12360ef803ee5667a36be6c7
Tin Stribor Sohn <54748826+TinStriborDodig@users.noreply.github.com>
2021-10-20 01:46:46 +0200

    added drawing

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 
 export default function Hero () {
-    return (<div style={{backgroundImage: "url(/eml.png)"}} class="container w-full  flex flex-col justify-center items-left px-8 pt-32 pb-32">
-        <div class="grid grid-cols-1 lg:grid-cols-2">
-            <div class="flex flex-col justify-center items-left">
-            <div class="font-bold text-xl lg:text-4xl">We move forward </div>
-        <div class="font-bold text-xl lg:text-4xl text-purple-500">Explainable Machine Learning</div>
-        <div class="font-bold text-xl lg:text-4xl text-purple-300">Zero-Shot Learning</div>
-        <div class="font-bold text-xl lg:text-4xl text-purple-200">Multimodal Learning</div>
+    return (<div class="container w-full  flex flex-col justify-center items-left px-8 pt-32 pb-32">
+        <div class="grid grid-cols-1 lg:grid-cols-5">
+            <div class="flex flex-col justify-center items-left col-span-2">
+            <div class="font-bold text-xl lg:text-3xl">We move forward </div>
+        <div class="font-bold text-xl lg:text-3xl text-purple-500">Explainable Machine Learning</div>
+        <div class="font-bold text-xl lg:text-3xl text-purple-300">Zero-Shot Learning</div>
+        <div class="font-bold text-xl lg:text-3xl text-purple-200">Multimodal Learning</div>
             </div>
-           <div >
-           <Image src="/eml.png" objectFit="cover" width="2000" height="2000"></Image>
+           <div class="col-span-3">
+           <Image src="/eml_unituebingen2.png" objectFit="contain" width="2000" height="1600"></Image>
            </div>
            
         </div> 
---
+++ pages/blogs.js
@@ -0,0 +1,3 @@
+export default function blogs(props) {
+    return (<></>);
+}
