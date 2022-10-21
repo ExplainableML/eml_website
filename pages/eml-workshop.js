eb1760c173f7ab08acaa53279bac7d69958c9d04
mancinimassimiliano <massimiliano.mancini8@gmail.com>
2022-10-21 17:39:17 +0200

    Added Michela Paganini to the workshop page

--- pages/eml-workshop.js
+++ pages/eml-workshop.js
@@ -91,6 +91,8 @@ rehypePlugins={[rehypeKatex, rehypeRaw]} skipHtml={false} escapeHtml={false} chi
 <h1>Speakers</h1>
     <div id="speakers" className="border-b-2 grid container grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-6 mb-2 text-center">
              <WorkshopMemberElement name="Trevor Darrell" img="/workshop/darrell.png" link="https://people.eecs.berkeley.edu/~trevor/" institute="UC Berkeley"></WorkshopMemberElement>
+             <WorkshopMemberElement name="Michela Paganini" img="/workshop/michela.png" link="https://mickypaganini.github.io/" institute="DeepMind"></WorkshopMemberElement>
+
     </div>
 <h1>Organizers</h1>
                         <div id="organizers" className="border-b-2 text-center grid container grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-6 mb-2">
