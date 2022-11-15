7eff62d09bc927e226ab49665eed58b6bc23341e
otniel <otimercea@gmail.com>
2022-11-15 20:11:15 +0100

    Put all teaching components on the same line

--- components/TeachingGrid.js
+++ components/TeachingGrid.js
@@ -1,7 +1,7 @@
 import TeachingCard from "./TeachingCard"
 export default function TeachingGrid (props) {
     return (
-        <div id="teaching" class="container grid gap-8 grid-cols-1 md:grid-cols-2 lg:gap-8 p-6 mb-16">
+        <div id="teaching" class="container grid gap-8 grid-cols-1 md:grid-cols-3 lg:gap-8 p-6 mb-16">
 { /*           <TeachingCard type="Courses"></TeachingCard>*/}
             <TeachingCard type="Seminars"></TeachingCard>  
             <TeachingCard type ="Master Thesis"></TeachingCard>
