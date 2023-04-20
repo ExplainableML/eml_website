5f2953eb57d7e0bbb743c4310e6729123c797a63
mancinimassimiliano <massimiliano.mancini8@gmail.com>
2023-04-20 11:08:18 +0200

    Changed title + fixed style inheritance

--- components/WorkshopCard.js
+++ components/WorkshopCard.js
@@ -12,17 +12,18 @@ export default function WorkshopCard(props) {
             setLink("courses")
         }
     },[])
-    return (<a href={"/"+link}><div class=" overflow-hidden shadow-lg bg-cover p-16 flex flex-col items-center justify-center"
+    return (<a href={"/"+link}><div>
+
+        {props.type == "XAI" && <div class=" overflow-hidden shadow-lg bg-cover p-16 flex flex-col items-center justify-center"
     style={{backgroundImage: "url('https://c.pxhere.com/photos/b8/4b/autumn_t_bingen_nature_fall_leaves_southern_germany_october_colorful_fall_color-845134.jpg!d')",
         height: "350px",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center"}}>
-
-        {props.type == "XAI" && <div>
-           <div className="font-bold text-center text-xl"     style={{ height: "100px", width: "800px", display: "table-cell", verticalAlign:"middle"}}>
+           <div className="font-bold text-center text-xl"     style={{height: "100px", width: "800px", display: "table-cell", verticalAlign:"middle"}}>
+               <p style={{  fontSize: "24pt", color:"white"}}>Workshop on </p>
                <p style={{  fontSize: "24pt", color:"white"}}>Explainability in Machine Learning</p>
 
-<p style={{  fontSize: "12pt", color:"white"}}> March 28-29th, 2023  |  Alte Aula, Tübingen, Germany</p></div>
+<p style={{  fontSize: "12pt", color:"white"}}> March 28-29th, 2023  |  Alte Aula, Tübingen, Germany</p></div>
 
            <div className="flex flex-row justify-center items-center pt-8">
                {/*<a class="text-purple-500 font-semibold" href="/eml-workshop">View Details</a>*/}
